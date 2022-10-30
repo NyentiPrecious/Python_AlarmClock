431ae1c9c048b46b509d31b293d19d0043b6211a
NyentiPrecious <gildanyenti@gmail.com>
2022-10-30 10:03:17 +0100

    alrm_clock

--- alarm/Clock/alarm_clock.py
+++ alarm/Clock/alarm_clock.py
@@ -1,51 +1,89 @@
-# Importing required libraries
-from datetime import datetime  # To set date and time
-from playsound import playsound  # To play sound
+# Import Required Library
+from tkinter import *
+import datetime
+import time
+import winsound
+from threading import *
+
+# Create Object
+root = Tk()
+
+# Set geometry
+root.geometry("400x200")
 
 
-def validate_time(alarm_time):
-    if len(alarm_time) != 11:
-        return "Invalid time format! Please try again..."
-    else:
-        if int(alarm_time[0:2]) > 12:
-            return "Invalid HOUR format! Please try again..."
-        elif int(alarm_time[3:5]) > 59:
-            return "Invalid MINUTE format! Please try again..."
-        elif int(alarm_time[6:8]) > 59:
-            return "Invalid SECOND format! Please try again..."
-        else:
-            return "ok"
+# Use Threading
+def Threading():
+    t1 = Thread(target=alarm)
+    t1.start()
 
 
-while True:
-    alarm_time = input("Enter time in 'HH:MM:SS AM/PM' format: ")
+def alarm():
+    # Infinite Loop
+    while True:
+        # Set Alarm
+        set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()}"
 
-    validate = validate_time(alarm_time.lower())
-    if validate != "ok":
-        print(validate)
-    else:
-        print(f"Setting alarm for {alarm_time}...")
-        break
+        # Wait for one seconds
+        time.sleep(1)
 
-alarm_hour = alarm_time[0:2]
-alarm_min = alarm_time[3:5]
-alarm_sec = alarm_time[6:8]
-alarm_period = alarm_time[9:].upper()
+        # Get current time
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time, set_alarm_time)
 
-while True:
-    now = datetime.now()
+        # Check whether set alarm is equal to current time or not
+        if current_time == set_alarm_time:
+            print("Time to Wake up")
+            # Playing sound
+            winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
 
-    current_hour = now.strftime("%I")
-    current_min = now.strftime("%M")
-    current_sec = now.strftime("%S")
-    current_period = now.strftime("%p")
 
-    from playsound import playsound
+# Add Labels, Frame, Button, Optionmenus
+Label(root, text="Alarm Clock", font=("Helvetica 20 bold"), fg="red").pack(pady=10)
+Label(root, text="Set Time", font=("Helvetica 15 bold")).pack()
 
-    if alarm_period == current_period:
-        if alarm_hour == current_hour:
-            if alarm_min == current_min:
-                if alarm_sec == current_sec:
-                    print("Wake Up!")
-                    playsound('/alarm/Clock/Alarm Clock_alarm.wav')
-                    break
+frame = Frame(root)
+frame.pack()
+
+hour = StringVar(root)
+hours = ('00', '01', '02', '03', '04', '05', '06', '07',
+         '08', '09', '10', '11', '12', '13', '14', '15',
+         '16', '17', '18', '19', '20', '21', '22', '23', '24'
+         )
+hour.set(hours[0])
+
+hrs = OptionMenu(frame, hour, *hours)
+hrs.pack(side=LEFT)
+
+minute = StringVar(root)
+minutes = ('00', '01', '02', '03', '04', '05', '06', '07',
+           '08', '09', '10', '11', '12', '13', '14', '15',
+           '16', '17', '18', '19', '20', '21', '22', '23',
+           '24', '25', '26', '27', '28', '29', '30', '31',
+           '32', '33', '34', '35', '36', '37', '38', '39',
+           '40', '41', '42', '43', '44', '45', '46', '47',
+           '48', '49', '50', '51', '52', '53', '54', '55',
+           '56', '57', '58', '59', '60')
+minute.set(minutes[0])
+
+mins = OptionMenu(frame, minute, *minutes)
+mins.pack(side=LEFT)
+
+second = StringVar(root)
+seconds = ('00', '01', '02', '03', '04', '05', '06', '07',
+           '08', '09', '10', '11', '12', '13', '14', '15',
+           '16', '17', '18', '19', '20', '21', '22', '23',
+           '24', '25', '26', '27', '28', '29', '30', '31',
+           '32', '33', '34', '35', '36', '37', '38', '39',
+           '40', '41', '42', '43', '44', '45', '46', '47',
+           '48', '49', '50', '51', '52', '53', '54', '55',
+           '56', '57', '58', '59', '60')
+second.set(seconds[0])
+
+secs = OptionMenu(frame, second, *seconds)
+secs.pack(side=LEFT)
+
+Button(root, text="Set Alarm", font=("Helvetica 15"), command=Threading).pack(pady=20)
+
+# Execute Tkinter
+root.mainloop()
